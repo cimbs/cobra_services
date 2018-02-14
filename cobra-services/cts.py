599c280bb5b2db836462b90c29028602b89d665f
Jorge Fernández de Cossío Díaz <j.cossio.diaz@gmail.com>
2018-02-14 13:11:00 -0500

    cts_brenda_to_kegg

---
+++ cobra-services/cts.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+
+def cts_brenda_to_kegg(brenda_name, max_requests=3):
+    """
+    Returns the KEGG id of a compound, from the name 
+    on BRENDA.
+    """
+
+    cts_response = requests.get('http://cts.fiehnlab.ucdavis.edu/'
+                                'service/convert/Chemical%20Name/'
+                                'KEGG/' + brenda_name)
+    request_counter = 0
+    while request_counter <= max_requests:
+        try:
+            kegg_ids = json.loads(cts_response.text)[0]['result']
+            break
+        except (KeyError, IndexError):
+            request_counter = request_counter + 1
+    else:
+        assert False
+
+    if kegg_ids:
+        return kegg_ids[0]
+    else:
+        return None
+
